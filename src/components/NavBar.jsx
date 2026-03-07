fa7b18e0f369e618c61f42b0852e5cb83303bc4d
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-07 21:56:55 +0000

    CSS module

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 export const NavBar = () => {
-    return <div> NavBar</div>;
+    return  (
+    
+    <nav>
+        <a href="/">Portfolio</a>
+        <div>
+            <ul>
+            <li> <a href="#about">About</a></li>
+            <li> <a href="#experience">Experience</a></li>
+            <li> <a href="#projects">Projects</a></li>
+            <li> <a href="#contact">Contact</a></li>
+            </ul>
+        </div>
 
+    </nav>
 
+);
 
 
     
