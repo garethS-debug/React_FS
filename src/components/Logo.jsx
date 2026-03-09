a5665bfe6a892cd6e0fe772261e87e3b120bdd52
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-09 23:15:24 +0000

    spring logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,30 @@
-import * as motion from "motion/react-client"
+// import { motion } from "motion/react"
+// import logo from '../../assets/Logo/logo_500px.svg'
+
+// export default function EnterAnimation() {
+//   return (
+//     <motion.img
+//       src={logo}
+//       initial={{ opacity: 0, scale: 0 }}
+//       animate={{ opacity: 1, scale: 1 }}
+//       transition={{
+//         duration: 0.4,
+//         scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+//       }}
+//       style={imgStyle}
+//       alt="logo"
+//     />
+//   )
+// }
+
+// const imgStyle = {
+//   width: 100,
+//   height: 100,
+//   borderRadius: "50%",
+//   objectFit: "cover",
+//   display: "block",
+// }
+import { motion } from "motion/react"
 import logo from '../../assets/Logo/logo_500px.svg'
 
 export default function EnterAnimation() {
