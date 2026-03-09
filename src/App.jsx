e3dc7bcfd695af37acf6744bb9d445aa39601cb1
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-09 20:52:24 +0000

    Updatig git ignore

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import styles from './App.module.css';
+
 import { NavBar } from './components/NavBar'
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   const [count, setCount] = useState(0)
