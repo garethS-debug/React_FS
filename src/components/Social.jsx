ae605efa3413e4086da6bf33a49d53390ab8cb07
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-10 23:38:44 +0000

    Importing moving boxes

---
+++ src/components/Social.jsx
@@ -0,0 +1,70 @@
+"use client"
+
+import * as motion from "motion/react-client"
+import { useEffect, useState } from "react"
+
+export default function Social() {
+    const [order, setOrder] = useState(initialOrder)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setOrder(shuffle(order)), 1000)
+        return () => clearTimeout(timeout)
+    }, [order])
+
+    return (
+        <ul style={container}>
+            {order.map((backgroundColor) => (
+                <motion.li
+                    key={backgroundColor}
+                    layout
+                    transition={spring}
+                    style={{ ...item, backgroundColor }}
+                />
+            ))}
+        </ul>
+    )
+}
+
+const initialOrder = [
+    "#ff0088",
+    "#dd00ee",
+    "#9911ff",
+    "#0d63f8",
+]
+
+/**
+ * ==============   Utils   ================
+ */
+function shuffle(array) {
+    return [...array].sort(() => Math.random() - 0.5)
+}
+
+/**
+ * ==============   Styles   ================
+ */
+
+const spring = {
+    type: "spring",
+    damping: 20,
+    stiffness: 300,
+}
+
+const container = {
+    listStyle: "none",
+    padding: 0,
+    margin: 0,
+    position: "relative",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: 10,
+    width: 300,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+}
+
+const item = {
+    width: 100,
+    height: 100,
+    borderRadius: "10px",
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 // HomePage.js
 import React from 'react';
-
+import Social from '../components/Social';
 //- **Homepage**: Introduction, links to social profiles, and an overview of your skills.
 
 const HomePage = () => {
@@ -8,6 +8,10 @@ const HomePage = () => {
     <div>
       <h2>Welcome to the Home Page</h2>
       <p>This is the main content of the home page.</p>
+      <p>Introduction</p>
+        <p>Links to social profiles</p>
+        <p>Overview of your skills</p>
+        <Social />
     </div>
   );
 };
