3ba9a73ad6da0d9e6ea724ba00b1ae4c228347cd
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-12 01:31:32 +0000

    Creating a repo card

---
+++ src/components/RepoCard.jsx
@@ -0,0 +1,31 @@
+
+import { motion } from "motion/react"
+import logo from '../../assets/Logo/logo_500px.svg'
+
+export default function EnterAnimation() {
+  return (
+    <motion.img
+      src={logo}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ rotate: 360, opacity: 1, scale: 1}}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5},
+      
+      }}
+       whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+    // animate2={{ rotate: 360 }}
+    //   transition2={{ duration: 1, ease: "easeInOut", repeat: Infinity, repeatType: "loop" }}
+      style={imgStyle}
+      alt="logo"
+    />
+  )
+}
+
+const imgStyle = {
+  width: 100,
+  height: 100,
+  objectFit: "cover",
+  display: "block",
+}
