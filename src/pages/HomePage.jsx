8c5c497b93ea86c80bdc36e3d52fe06f68efe630
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-13 13:26:17 +0000

    RemovingFaancyText

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Social from '../components/Social';
 import styles from './HomePage.module.css';
 import Drawing from '../components/Drawing';
 import AnimatedText from '../components/AnimatedText';
-import TextRotate from '../components/text-rotate';
+// import TextRotate from '../components/text-rotate';
 
 const HomePage = () => {
   return (
@@ -12,11 +12,11 @@ const HomePage = () => {
       <section className={styles.hero}>
         <div className={styles.heroLeft}>
           {/* <h1 className={styles.title}>Building Digital products and experiences</h1> */}
-      <TextRotate
+      {/* <TextRotate
         texts={['Rotating', 'Text']}
         mainClassName="text-white px-2 sm:px-2 md:px-3 bg-[#ff5941] overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
         rotationInterval={2000}
-      />
+      /> */}
         </div>
         <div className={styles.heroRight}>
          
