65af9e27a2bf37da4d3c5ea7b649c8e230b34450
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-13 14:47:59 +0000

    Finally Finished the reactive text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,18 +12,22 @@ const HomePage = () => {
       <section className={styles.hero}>
         <div className={styles.heroLeft}>
 
-<RotatingText
-   texts={['React', 'Bits', 'Is', 'Cool!']}
-  mainClassName="rotating-hero px-2 sm:px-2 md:px-3 bg-transparent text-white overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-  staggerFrom={"last"}
-  initial={{ y: "100%" }}
-  animate={{ y: 0 }}
-  exit={{ y: "-120%" }}
-  staggerDuration={0.025}
-  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-  transition={{ type: "spring", damping: 30, stiffness: 400 }}
-  rotationInterval={2000}
-/>
+<span className="rotating-line">
+  <span className="rotating-before">Creative</span>
+  <RotatingText
+    texts={['React', 'Bits', 'Is', 'Cool!']}
+    mainClassName="rotating-hero px-2 sm:px-2 md:px-3 bg-transparent overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+    staggerFrom={"last"}
+    initial={{ y: "100%" }}
+    animate={{ y: 0 }}
+    exit={{ y: "-120%" }}
+    staggerDuration={0.025}
+    splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+    transition={{ type: "spring", damping: 30, stiffness: 400 }}
+    rotationInterval={2000}
+  />
+  <span className="rotating-after">thinking</span>
+</span>
 
 
           
