685ab17ad7708f40063a02e3471d7e43f42c702b
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-14 00:45:17 +0000

    Fixing deploy issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: ['react-fs-1.onrender.com', 'localhost']
+  }
 })
